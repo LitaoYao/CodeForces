08ebe8f685a7253c3bfb14da9b28576fea5bd72b
LitaoYao <yaobob@foxmail.com>
2022-05-13 09:56:26 +0800

    Try solve a simple problem of Edu Contest in the pass.

---
+++ Educational Contest#127(Div.2)/A.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void solve()
+{
+	string str;
+	cin >> str;
+	bool ans = true;
+	int n = str.size();
+	if (n == 1)
+	{
+		ans = false;
+	}
+	else
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			if (i == 0 && str[i] != str[i + 1])
+			{
+				ans = false;
+				break;
+			}
+			else if (i == n - 1 && str[i] != str[i - 1])
+			{
+				ans = false;
+				break;
+			}
+			else if (str[i] != str[i - 1] && str[i] != str[i + 1])
+			{
+				ans = false;
+				break;
+			}
+		}
+	}
+	if (ans)
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
