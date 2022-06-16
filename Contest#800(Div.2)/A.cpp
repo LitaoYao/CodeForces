05d9f31125ae8c681d841e8c2fa65236c1bf6702
LitaoYao <yaobob@foxmail.com>
2022-06-17 00:47:33 +0800

    fucking B!!!

---
+++ Contest#800(Div.2)/A.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void fill(string& str, int a, int b, char A, char B)
+{
+	int maxScore = max(1, a - b);
+	while (a >= maxScore && b >= maxScore)
+	{
+		for (int i = 0; i < maxScore; ++i)
+		{
+			str += B;
+		}
+		for (int i = 0; i < maxScore; ++i)
+		{
+			str += A;
+		}
+		a -= maxScore;
+		b -= maxScore;
+	}
+	for (int i = 0; i < b; ++i)
+	{
+		str += B;
+	}
+	for (int i = 0; i < a; ++i)
+	{
+		str += A;
+	}
+}
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+	string str = "";
+	if (a > b)
+	{
+		fill(str, a, b, '0', '1');
+	}
+	else
+	{
+		fill(str, b, a, '1', '0');
+	}
+	cout << str << endl;
+	return;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
---
+++ Contest#800(Div.2)/B.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+void solve()
+{
+	int n;
+	cin >> n;
+	string str;
+	cin >> str;
+	long long ans = n;
+	for (int i = 1; i < n; ++i)
+	{
+		if (str[i] != str[i - 1])
+		{
+			ans += i;
+		}
+	}
+	cout << ans << endl;
+	return;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
